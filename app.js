a4d11e10c58caf6874b4b64b158366ee3a3baf38
LilHoneyBunny <zs19014025@gmail.com>
2025-11-18 15:59:50 -0600

    para desplegar en AWS

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Holi desde mi app Node.js!");
+const express = require("express");
+const app = express();
+
+// Ruta principal
+app.get("/", (req, res) => {
+  res.send("Holi desde mi app Node.js corriendo en AWS con Docker!");
+});
+
+// Puerto (AWS usará este)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
